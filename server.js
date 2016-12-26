bd3cf777f710348321b196aee1cfa950a2ce7c74
Ann George <anngrg126@gmail.com>
2016-12-25 22:38:18 -0600

    Can add users

--- server.js
+++ server.js
@@ -4,6 +4,34 @@ var app = express();
 app.use(express.static("./client"));
 app.use(express.static("./node_modules"));
 
+app.use(require("body-parser").json());
+
+var mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/survey-test-app3");
+mongoose.Promise = global.Promise;
+
+var userSchema = new mongoose.Schema({
+  username: {type: String, unique: true, required: true},
+}, {timestamps: true});
+
+mongoose.model("User", userSchema);
+var User = mongoose.model("User");
+
+app.post("/users", function(req, res){
+  console.log(req.body.username);
+
+  var user = new User({username: req.body.username});
+
+  user.save(function(error){
+    if(error) {
+      console.log(error);
+      res.json({ error: error });
+    }
+    res.json ({user: user});
+  });
+
+});
+
 app.listen(8000, function() {
   console.log("Listening on 8000");
 });
